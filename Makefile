f3c9730848a7bab87874bb257cf98d8345ede538
Yohan BENDAVID <ybendavi@e1r4p10.42.fr>
2022-06-01 19:06:56 +0200

    project started

---
+++ Makefile
@@ -0,0 +1,25 @@
+NAME = minishell
+CC = gcc
+RM = rm -rf
+SRCSDIR = srcs
+OBJSDIR = objs
+SRCS = main.c 
+OBJS = ${SRCS:%.c=${OBJSDIR}/%.o}
+$(NAME): ${OBJS}
+	$(CC) $(OBJS) -Wall -Wextra -Werror -o ${NAME}
+${OBJS}: ${OBJSDIR}/%.o: ${SRCSDIR}/%.c
+	mkdir -p ${OBJSDIR}
+	$(CC) -Wall -Wextra -Werror -I./includes -g -c $< -o $@
+
+all: ${NAME}
+
+clean: 
+	${RM} ${OBJS}
+	${RM} ${OBJSDIR}
+
+fclean: clean
+	${RM} ${NAME}
+
+re: fclean all
+
+.PHONY: clean all fclean re
---
+++ includes/minishell.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybendavi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/01 18:46:40 by ybendavi          #+#    #+#             */
+/*   Updated: 2022/06/01 19:04:48 by ybendavi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <stdio.h>
+
+typedef enum e_token_type {
+	REDIR_ADD,
+	REDIR_LIM,
+	REDIR_IN,
+	REDIR_OUT,
+	PIPE,
+	QUOTE,
+	S_QUOTE,
+	WHITE_SPACE,
+	STR,
+	FLAG,
+	CMD
+}		t_token_type;
+
+typedef struct s_token {
+	char			*token;
+	uint32_t		size;
+	t_token_type	type;
+}	t_token;
+
+typedef struct s_lexed {
+	t_token		*tokens;
+	uint32_t	size;
+}			t_lexed;
+
+typedef struct s_env {
+	t_token	*tab;
+	t_lexed	*lexed;
+}			t_env;
+
+typedef struct command_table_list
+{
+	char						**cmd;
+	int							in;
+	int							out;
+	struct command_table_list	*next;
+}					t_cmds;
+
+#endif
