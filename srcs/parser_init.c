d73c483660cc555dc49f09a577b2c82f47dfd42c
ccottin <cottin.clara7@gmail.com>
2022-06-12 23:55:47 +0200

    add null token

--- srcs/parser_init.c
+++ srcs/parser_init.c
@@ -20,7 +20,7 @@ int	get_parsed(t_env *data, t_token token)
 		data->parsed[data->nb_parsed].token = ft_cpy2(token.token);
 	if (!data->parsed[data->nb_parsed].token)
 		return (-1);
-	data->parsed[data->nb_parsed].size = token.size;
+	data->parsed[data->nb_parsed].size = token.size;//
 	data->parsed[data->nb_parsed].type = token.type;
 	data->nb_parsed++;
 	return (0);
@@ -73,6 +73,16 @@ int	check_type(unsigned int *i, t_env *data, char **env)
 	return (0);
 }
 
+void	add_null_token(t_env *data)
+{
+	t_token	token;
+
+	token.token = NULL;
+	token.type = STR;
+	token.size = 0;
+	data->parsed[data->nb_parsed] = token;
+}
+
 int	init_parser(t_env *data, char **env)
 {
 	unsigned int	i;
@@ -92,6 +102,7 @@ int	init_parser(t_env *data, char **env)
 		}
 		i++;
 	}
+	add_null_token(data);
 	free_lexed(data);
 	return (0);
 }
