456940b61caff12e9d705656a892bf77ef405ddb
Clara COTTIN <ccottin@e1r11p4.42.fr>
2022-06-28 18:20:29 +0200

    fix echo

--- srcs/builtin/ft_echo.c
+++ srcs/builtin/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: ccottin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 19:44:25 by ccottin           #+#    #+#             */
-/*   Updated: 2022/06/28 13:43:07 by ccottin          ###   ########.fr       */
+/*   Updated: 2022/06/28 18:10:35 by ccottin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,20 @@ int	ft_echo(char **strs)
 	if (!strs)
 		return (0);
 	mark = 0;
-	if (ft_strncmp(strs[1], "-n", 2))
+	i = 1;
+	while (!(ft_strncmp(strs[i], "-n", 2)))
+	{
 		mark = 1;
-	i = 1 + mark;
+		i++;
+	}
 	while (strs[i])
 	{
 		ret = write(1, strs[i], ft_strlen(strs[i]));
-		if (ret)
-			return (ret);//? + rajouter un espace entre chaqu
+		if (strs[i + 1])
+			write(1, " ", 1);
 		i++;
 	}
-	if (mark)
+	if (!mark)
 		write(1, "\n", 1);
 	return (0);
 }
