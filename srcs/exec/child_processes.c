b4cb64fa4831b09a7afa1df220397d42a94ba5cb
ccottin <cottin.clara7@gmail.com>
2022-07-05 17:54:17 +0200

    reboot

--- srcs/exec/child_processes.c
+++ srcs/exec/child_processes.c
@@ -6,7 +6,7 @@
 /*   By: ybendavi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 18:38:51 by ybendavi          #+#    #+#             */
-/*   Updated: 2022/07/01 01:18:19 by ybendavi         ###   ########.fr       */
+/*   Updated: 2022/06/30 23:25:34 by ccottin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int	no_builtin(t_cmds *cmd, char **env, t_env *envs, int ret)
 	if (ret == 127)
 	{
 		errno = 2;
-		write(2, "bash: ", ft_strlen("bash: ");
+		write(2, "bash: ", ft_strlen("bash: "));
 		exit(errno_two(cmd->cmd, envs));
 	}
 	if (cmd->cmd && is_builtin(cmd) == 0)
