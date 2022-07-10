2d2f5f6efc968c66dda13437542375ae00966188
Yohan BENDAVID <ybendavi@e1r4p19.clusters.42paris.fr>
2022-07-10 19:15:43 +0200

    testing

--- srcs/builtin/ft_exit2.c
+++ srcs/builtin/ft_exit2.c
@@ -6,11 +6,29 @@
 /*   By: ybendavi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 15:49:28 by ybendavi          #+#    #+#             */
-/*   Updated: 2022/07/10 15:52:43 by ybendavi         ###   ########.fr       */
+/*   Updated: 2022/07/10 18:06:16 by ybendavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	exit_write(t_env *data)
+{
+	t_cmds	*tmp;
+
+	tmp = data->c_tbls;
+	while (tmp && tmp->cmd
+		&& ft_strncmp(tmp->cmd, "exit",
+		ft_strlen(tmp->cmd)) != 0)
+	{
+		tmp = tmp->next;
+	}
+	if (tmp)
+	{
+		if (tmp->pfd_out[1] != -1)
+			write(1, "exit\n", 5);
+	}
+}
 
 void	exit_fd(int in, int out)
 {
